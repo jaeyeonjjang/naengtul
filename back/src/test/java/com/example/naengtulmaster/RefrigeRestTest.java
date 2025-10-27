ae7ea72e9cc63a3260302df5383efe050b83a885
jaeyeonjjang <wodus0936@gmail.com>
2025-10-27 23:34:56 +0900

    JWT 인증기능 JUnit 테스트

---
+++ back/src/test/java/com/example/naengtulmaster/RefrigeRestTest.java
@@ -0,0 +1,31 @@
+package com.example.naengtulmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+
+public class RefrigeRestTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testAuthentication() throws Exception {
+        // 올바른 자격 증명으로 인증 테스트
+        this.mockMvc.
+                perform(post("/login").
+                content("{\"username\":\"admin\",\"password\":\"admin\"}").
+                header(HttpHeaders.CONTENT_TYPE, "application/json")).
+                andDo(print()).andExpect(status().isOk());
+    }
+
+}
